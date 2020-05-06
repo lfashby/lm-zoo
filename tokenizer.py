fcc504f347641e9eb4add343ec5dcb9c0429ef17
Lucas Ashby <lfeashby@gmail.com>
2020-05-06 19:49:00 -0400

    Experimenting with flag parsing and basic structure of eventual bash script

---
+++ tokenizer.py
@@ -0,0 +1,11 @@
+import nltk
+import sys
+
+print(sys.argv[1])
+
+text_file = sys.argv[1]
+
+with open (text_file, "r") as text:
+    with open(text_file.replace("txt", "tok"), "w") as sink:
+         for line in text:
+             print(" ".join(nltk.word_tokenize(line)), file=sink)
